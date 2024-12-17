9faeb1833595c92d256dec1bcba859cd19aba60d
lkcina <lkcina6@gmail.com>
2024-12-17 13:41:11 -0500

    Create React and ASP.NET Core Web Api project. Add User Model.

---
+++ Qsound/Qsound.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Qsound.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public int? SubscriptionType { get; set; }
+        public List<Project> Projects { get; set; }
+
+    }
+}
